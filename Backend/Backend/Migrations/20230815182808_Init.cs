a6fcdaa4e74bd7e52a8ff2a71667d021aa1c02d9
Elcasvi <casvi.sanchez@gmail.com>
2023-08-15 12:31:31 -0600

    Setting up the join tables in the DbContext and setting up EF completed

---
+++ Backend/Backend/Migrations/20230815182808_Init.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Skill = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobOffers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Enterprise = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobOffers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssignedJobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Enterprise = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignedJobs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssignedJobs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCourses",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCourses", x => new { x.UserId, x.CourseId });
+                    table.ForeignKey(
+                        name: "FK_UserCourses_Courses_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCourses_Users_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserJobOffers",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    JobOfferId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserJobOffers", x => new { x.UserId, x.JobOfferId });
+                    table.ForeignKey(
+                        name: "FK_UserJobOffers_JobOffers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "JobOffers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserJobOffers_Users_JobOfferId",
+                        column: x => x.JobOfferId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignedJobs_UserId",
+                table: "AssignedJobs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCourses_CourseId",
+                table: "UserCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserJobOffers_JobOfferId",
+                table: "UserJobOffers",
+                column: "JobOfferId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignedJobs");
+
+            migrationBuilder.DropTable(
+                name: "UserCourses");
+
+            migrationBuilder.DropTable(
+                name: "UserJobOffers");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "JobOffers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Backend/Backend/Models/Data/AppDbContext.cs
+++ Backend/Backend/Models/Data/AppDbContext.cs
@@ -10,11 +10,45 @@ namespace Backend.Models.Data
         {
 
         }
+        //Entities
         public DbSet<User> Users { get; set; }
         public DbSet<JobOffer> JobOffers { get; set; }
         public DbSet<AssignedJob> AssignedJobs { get; set; }
         public DbSet<Course> Courses { get; set; }
+        
+        
+        //Join tables
         public DbSet<UserCourse> UserCourses { get; set; }
         public DbSet<UserJobOffer> UserJobOffers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserCourse>().HasKey(usercourse => new { usercourse.UserId, usercourse.CourseId });
+            
+            modelBuilder.Entity<UserCourse>()
+                .HasOne(user => user.User)
+                .WithMany(usercourse => usercourse.UserCourses)
+                .HasForeignKey(course => course.CourseId);
+            
+            modelBuilder.Entity<UserCourse>()
+                .HasOne(course => course.Course)
+                .WithMany(usercourse => usercourse.UserCourses)
+                .HasForeignKey(user => user.UserId);
+            
+            
+            
+            modelBuilder.Entity<UserJobOffer>().HasKey(userjoboffer => new { userjoboffer.UserId, userjoboffer.JobOfferId });
+            
+            modelBuilder.Entity<UserJobOffer>()
+                .HasOne(user => user.User)
+                .WithMany(userjoboffer => userjoboffer.UserJobOffers)
+                .HasForeignKey(joboffer => joboffer.JobOfferId);
+            
+            modelBuilder.Entity<UserJobOffer>()
+                .HasOne(joboffer => joboffer.JobOffer)
+                .WithMany(userjoboffer => userjoboffer.UserJobOffers)
+                .HasForeignKey(user => user.UserId);
+
+        }
     }
 }
