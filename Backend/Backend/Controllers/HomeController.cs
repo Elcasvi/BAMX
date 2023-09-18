fbda75e6920acc4edde1c2ae8a3293f27beec9c2
Elcasvi <casvi.sanchez@gmail.com>
2023-09-17 19:54:44 -0600

    I deploy the restful api to an azure web service

---
+++ Backend/Backend/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController
+    {
+
+        [HttpGet]
+        public string Home()
+        {
+            return "Welcome to BAMX Backend";
+        }
+    }
+}
