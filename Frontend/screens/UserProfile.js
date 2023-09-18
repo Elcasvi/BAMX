22cb838a009b08b64041579b6ad8d2aa9c90f00c
Elcasvi <casvi.sanchez@gmail.com>
2023-09-18 10:44:32 -0600

    I do a http request for my user profile

--- Frontend/screens/UserProfile.js
+++ Frontend/screens/UserProfile.js
@@ -1,9 +1,23 @@
 import {Text, View} from "react-native";
+import {useEffect, useState} from "react";
 
 export default function UserProfile({navigation}) {
+
+    const url = "https://bamx.azurewebsites.net/user/1"
+    const[data,setData]=useState({})
+    const [loading,setLoading]=useState(false)
+    useEffect(() => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>User profile!</Text>
+            <Text style={{color:"#FFF"}}>User profile!</Text>
+            <Text style={{color:"#FFF"}}>{data.name}</Text>
 
         </View>
     );
