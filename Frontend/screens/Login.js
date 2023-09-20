b4e5b7fa32b69c33037d7d634f2094be85f6e7e9
Elcasvi <casvi.sanchez@gmail.com>
2023-09-20 11:52:17 -0600

    I handle the bad request exceptions thrown in the loing fetch call

--- Frontend/screens/Login.js
+++ Frontend/screens/Login.js
@@ -9,19 +9,47 @@ export default function Login()
     const[user,setUser]=useState({})
     const [loading,setLoading]=useState(false)
 
-   function handleLoginBtn()
-    {
-       console.log("Hi console")
+    function handleLoginBtn() {
+        console.log("Hi console");
         fetch(url)
-            .then((resp) => resp.json())
-            .then(data =>{
-                AsyncStorageNative.setItem("KeepLoggedIn",JSON.stringify(true))
-                AsyncStorageNative.setItem('token',JSON.stringify(data.data))
-                setUser(data)
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return resp.json();
             })
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
+            .then((data) => {
+                AsyncStorageNative.setItem("KeepLoggedIn", JSON.stringify(true))
+                    .then(() => {
+                        // Code to execute after successfully setting the item
+                        console.log("KeepLoggedIn item set successfully");
+                    })
+                    .catch((error) => {
+                        // Handle any errors that occur during the set operation
+                        console.error("Error setting KeepLoggedIn item:", error);
+                    });
+
+                AsyncStorageNative.setItem('token', JSON.stringify(data))
+                    .then(() => {
+                        // Code to execute after successfully setting the 'token' item
+                        console.log("'token' item set successfully");
+                    })
+                    .catch((error) => {
+                        // Handle any errors that occur during the set operation
+                        console.error("Error setting 'token' item:", error);
+                    });
+
+                setUser(data);
+            })
+            .catch((error) => {
+                // Handle fetch errors
+                console.error("Fetch error:", error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }
+
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
