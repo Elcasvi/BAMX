f06623f3f7a6d80acdfb3fbd1bbec49786143abb
Elcasvi <casvi.sanchez@gmail.com>
2023-09-21 20:17:57 -0600

    I start working on the apply job feature

--- Frontend/screens/Jobs/JobDetailsScreen.js
+++ Frontend/screens/Jobs/JobDetailsScreen.js
@@ -1,9 +1,13 @@
-import {SafeAreaView, Text, View} from "react-native";
+import {Button, SafeAreaView, Text, View} from "react-native";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import JobContent from "../../components/Jobs/JobContent";
-import {useLayoutEffect} from "react";
+import {useContext, useLayoutEffect} from "react";
+import {AuthContext} from "../../context/AuthContext";
+import {BASE_URL} from "../../config";
+import axios from "axios";
 
 export default function JobDetailsScreen() {
+    const {userInformation}=useContext(AuthContext)
     const navigation=useNavigation();
     const route=useRoute();
     const{params}=route;
@@ -15,9 +19,37 @@ export default function JobDetailsScreen() {
             })
         }
         ,[])
+
+    const handelApplyBtn=()=>
+    {
+        console.log("in handleApplyBtn")
+        const userBody={
+            Name:userInformation.name,
+            Email:userInformation.email,
+            Password:userInformation.password,
+            Role:userInformation.role,
+            Gender:userInformation.gender,
+            Rating:userInformation.rating,
+            ProfilePic:userInformation.profilePic
+        }
+        const url=BASE_URL+"/update/"+params.job.id
+        axios.post(url,userBody)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch((error) => {
+                alert("Error: "+error)
+            })
+    }
+
     return(
-        <SafeAreaView>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>{params.job.id}</Text>
             <JobContent job={params.job}/>
-        </SafeAreaView>
+
+            {userInformation.role==="admin"?
+                <Button title="See users apliying"/>:
+                <Button title="Aply" onPress={handelApplyBtn}/>}
+        </View>
     )
 }
