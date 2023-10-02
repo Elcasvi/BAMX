c12d99f585006641be2b7b65de861e2a0e96a345
leviptz <levipatzwaldt@icloud.com>
2023-10-02 07:52:58 -0600

    implemented logo

--- Frontend/screens/Login.js
+++ Frontend/screens/Login.js
@@ -1,9 +1,10 @@
-import { useColorScheme, View} from "react-native";
+import { Image, useColorScheme, View} from "react-native";
 import { Text, TextInput, Button } from 'react-native-paper';
 import {useContext, useEffect, useState} from "react";
 import AsyncStorageNative from "@react-native-async-storage/async-storage/src/AsyncStorage.native";
 import {useNavigation} from "@react-navigation/native";
 import {AuthContext} from "../context/AuthContext";
+import logo from "../lib/images/bamx.png"
 
 export default function Login()
 {
@@ -20,11 +21,15 @@ export default function Login()
         navigation.navigate("Register")
     }
     return (
-        <View style={{ flex: 1, alignItems: 'center', padding: 40 }}>
+        <View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
+            <Image
+                style={{ width: 100, height: 100 }}
+                source={logo}
+            />
             <TextInput style={{width: "100%", margin: 10 }} mode="outlined" placeholder={"email"} onChangeText={setEmail} value={email}/>
             <TextInput secureTextEntry={true} style={{width: "100%" }} mode="outlined" placeholder={"password"} onChangeText={setPassword} value={password}/>
             <Button style={{width: "50%", margin: 10 }} mode="contained" onPress={()=>login({email,password})}>Login</Button>
-            <Button style={{width: "50%" }} mode="contained-tonal" onPress={goToRegister}>Register</Button>
+            <Button style={{width: "50%" }} mode="outlined" onPress={goToRegister}>Register</Button>
         </View>
     );
 };
--- Frontend/screens/Register.js
+++ Frontend/screens/Register.js
@@ -1,10 +1,10 @@
-import { SafeAreaView, useColorScheme, View} from "react-native";
+import { Image, SafeAreaView, useColorScheme, View} from "react-native";
 import { Text, TextInput, Button } from 'react-native-paper';
 import {useContext, useEffect, useState} from "react";
 import AsyncStorageNative from "@react-native-async-storage/async-storage/src/AsyncStorage.native";
 import {useNavigation} from "@react-navigation/native";
 import {AuthContext} from "../context/AuthContext";
-
+import logo from "../lib/images/bamx.png"
 
 export default function Register()
 {
@@ -29,11 +29,15 @@ export default function Register()
         register({userBody})
     }
     return(
-        <View style={{ flex: 1, alignItems: 'center', padding: 40 }}>
+        <View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
+            <Image
+                style={{ width: 100, height: 100 }}
+                source={logo}
+            />
             <TextInput style={{width: "100%", margin: 10 }} mode="outlined" placeholder={"email"} onChangeText={setEmail} value={email}/>
             <TextInput secureTextEntry={true} style={{width: "100%" }} mode="outlined" placeholder={"password"} onChangeText={setPassword} value={password}/>
             <TextInput style={{width: "100%", margin: 10 }} mode="outlined" placeholder={"name"} onChangeText={setName} value={name}/>
-            <Button style={{width: "50%" }} mode="contained-tonal" onPress={handleRegisterBtn}>Register</Button>
+            <Button style={{width: "50%" }} mode="contained" onPress={handleRegisterBtn}>Register</Button>
         </View>
     );
 };
