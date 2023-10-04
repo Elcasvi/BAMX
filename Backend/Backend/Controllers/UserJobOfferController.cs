89b39026a3ab01176b5a37ed439cd81bddc9ca4e
Elcasvi <casvi.sanchez@gmail.com>
2023-10-04 08:31:51 -0600

    I implement the deleteUserJobOffer endpoint

--- Backend/Backend/Controllers/UserJobOfferController.cs
+++ Backend/Backend/Controllers/UserJobOfferController.cs
@@ -1,4 +1,5 @@
 ﻿using Backend.Models.Entities;
+using Backend.Models.Entities.JoinTables;
 using Backend.Models.Interfaces;
 using Backend.Models.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +11,13 @@ public class UserJobOfferController:ControllerBase
 {
     private readonly IUserJobOfferRepository _userJobOfferRepository;
     private readonly IJobOfferRepository _jobOfferRepository;
+    private readonly IUserRepository _userRepository;
 
-    public UserJobOfferController(IUserJobOfferRepository userJobOfferRepository, IJobOfferRepository jobOfferRepository)
+    public UserJobOfferController(IUserJobOfferRepository userJobOfferRepository, IJobOfferRepository jobOfferRepository,IUserRepository userRepository)
     {
         _userJobOfferRepository = userJobOfferRepository;
         _jobOfferRepository = jobOfferRepository;
+        _userRepository = userRepository;
     }
 
     [HttpGet("{jobOfferId}")]
@@ -32,5 +35,33 @@ public class UserJobOfferController:ControllerBase
         
         return Ok(_userJobOfferRepository.GetAllUsersByJobOfferId(jobOfferId));
     }
-    
+
+    [HttpDelete("{jobOfferId}/{userId}")]
+    public IActionResult DeleteUserJobOffer(int jobOfferId, int userId)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        if (!_userRepository.Exists(userId))
+        {
+            return NotFound(userId);
+        }
+        User user = _userRepository.Get(userId);
+        
+        if (!_jobOfferRepository.Exists(jobOfferId))
+        {
+            return NotFound(jobOfferId);
+        }
+        JobOffer jobOffer = _jobOfferRepository.Get(jobOfferId);
+        UserJobOffer userJobOffer = new UserJobOffer()
+        {
+            UserId = userId,
+            JobOfferId = jobOfferId,
+            User = user,
+            JobOffer = jobOffer
+        };
+       var deletedUserJobOffer= _userJobOfferRepository.DeleteUserJobOffer(userJobOffer);
+       return Ok(deletedUserJobOffer);
+    }
 }
