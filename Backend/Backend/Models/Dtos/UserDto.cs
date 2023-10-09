6514f468183ae016a24f27a3a207d4ee45d3798f
Elcasvi <casvi.sanchez@gmail.com>
2023-10-09 15:52:14 -0600

    Some new properties for user have been added

--- Backend/Backend/Models/Dtos/UserDto.cs
+++ Backend/Backend/Models/Dtos/UserDto.cs
@@ -8,6 +8,9 @@ public class UserDto
     public string Role { get; set; }
     public string Gender { get; set; }
     public float Rating { get; set; }
+    public string? Description { get; set; }
+    public string? CVUrl { get; set; }
+    public string? NameOfCV { get; set; }
     public string? ProfilePictureUrl { get; set; }
     public string? NameOfProfilePicture { get; set; }
 }
--- Backend/Backend/Models/Entities/User.cs
+++ Backend/Backend/Models/Entities/User.cs
@@ -13,6 +13,9 @@ namespace Backend.Models.Entities
         public string Role { get; set; }
         public string Gender { get; set; }
         public float Rating { get; set; }
+        public string? Description { get; set; }
+        public string? CVUrl { get; set; }
+        public string? NameOfCV { get; set; }
         public string? ProfilePictureUrl { get; set; }
         public string? NameOfProfilePicture { get; set; }
         public ICollection<AssignedJob>?AssignedJobs { get; set; }
