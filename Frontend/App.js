89ad316020d7b00282d67ac2c38c6f3aff80afb6
leviptz <levipatzwaldt@icloud.com>
2023-10-12 11:24:06 -0600

    implemented react kittens ui

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,30 +1,17 @@
 import 'react-native-gesture-handler';
 import AppNavigation from "./navigation/AppNavigation";
-import {DarkTheme, DefaultTheme as DefaultThemeReact, NavigationContainer} from "@react-navigation/native";
-import {StatusBar} from "expo-status-bar";
-import LoginScreenStackNavigator from "./navigation/LoginScreenStackNavigator";
-import {useColorScheme} from "react-native";
-import {useEffect, useState} from "react";
-import AsyncStorageNative from "@react-native-async-storage/async-storage/src/AsyncStorage.native";
-import {
-    MD3LightTheme as DefaultTheme,
-    PaperProvider,
-  } from 'react-native-paper';
-import theme from './lib/theme.json'
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import {AuthProvider} from "./context/AuthContext";
 
-const theming = {
-    ...DefaultTheme,
-    colors: theme.colors,
-  };
-
 export default function App() {
-  const currentTheme=useColorScheme();
   return (
       <AuthProvider>
-        <PaperProvider theme={theming}>
-          <AppNavigation/>
-        </PaperProvider>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <AppNavigation/>
+        </ApplicationProvider>
       </AuthProvider>
 );
 }
